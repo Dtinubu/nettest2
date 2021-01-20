3b5b1502ffa2dc87bcc57534cda220b6e353461c
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-20 04:43:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ def main(args):
 
 
 def get_dataset_dir(args):
-    home = os.path.expanduser("~")
+    home = os.path.expanduser("/cmlscratch/dtinubu")
     dataset_dir = args.dataset_dir if args.dataset_dir else os.path.join(
-        home, 'datasets', 'lfw')
-
+        home, 'datasets', 'LFW')
+    
     if not os.path.isdir(dataset_dir):
         os.mkdir(dataset_dir)
 
